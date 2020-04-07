2d495e0d02e0901a40c57c383b15761de6aedae9
arvvm <39861844+arvvm@users.noreply.github.com>
2020-04-07 20:55:09 +0530

    Add files via upload

---
+++ face detection/DatasetCreater.py
@@ -0,0 +1,38 @@
+import cv2
+import sqlite3
+import numpy as np
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
+cam=cv2.VideoCapture(0);
+def insertOrUpdate(Id,Name):
+    conn=sqlite3.connect("face -b.db")
+    cmd="SELECT *FROM Info WHERE Id="+str(Id)
+    cursor=conn.execute(cmd)
+    isRecordExist=1
+    for row in cursor:
+        isRecordExist=1
+    if(isRecordExist==1):
+        cmd="UPDATE People SET Name="+str(Name)+"WHERE Id="+str(Id)
+    else:
+        cmd="INSERT INTO People(id,Name) Values("+str(Id)+", "+str(Name)+")"
+        conn.execute(cmd)
+        conn.commit()
+        conn.close()
+        id=input('enter user id :')
+        Name=input('enter user name :')
+        insertOrUpdate(Id,Name)
+        sampleNum=0;
+    while(True):
+    ret,img=cam.read();
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=faceDetect.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        sampleNum=sampleNum+1;
+        cv2.imwrite("dataset/"+str(id)+"."+str(sampleNum)+".jpg", gray[y:y+h, x:x+w])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,2525),2)
+        cv2.waitKey(100);
+        cv2.imshow("frame",img);
+        cv2.waitKey(1);
+    if sampleNum>10:
+        break
+cam.release()
+cv2.destroyAllWindows()
